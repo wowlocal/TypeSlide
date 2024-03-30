565ec717c7eda8f48f9e7931c48723b4e7c7a874
Mikhail <wowloca1@yandex.ru>
2024-03-30 19:55:00 +0300

    Initial Commit

---
+++ TypeSlide/TypeSlideApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeSlideApp.swift
+//  TypeSlide
+//
+//  Created by Misha Nya on 30.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TypeSlideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
