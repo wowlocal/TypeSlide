076dbc397a0a3f71a2dcd819ade47a5cd0af4143
Mikhail <wowloca1@yandex.ru>
2024-04-16 21:15:13 +0300

    frutia compiles

---
+++ TypeSlide/Samples/FrutiaTransition.swift
@@ -0,0 +1,413 @@
+//
+//  FrutiaTransition.swift
+//  TypeSlide
+//
+//  Created by Misha Nya on 16.04.2024.
+//
+
+import SwiftUI
+
+struct AnimatableFontModifier: AnimatableModifier {
+	var size: Double
+	var weight: Font.Weight = .regular
+	var design: Font.Design = .default
+
+	var animatableData: Double {
+		get { size }
+		set { size = newValue }
+	}
+
+	func body(content: Content) -> some View {
+		content
+			.font(.system(size: size, weight: weight, design: design))
+	}
+}
+
+extension Ingredient {
+
+	/// Defines how the `Ingredient`'s title should be displayed in card mode
+	struct CardTitle {
+		var color = Color.black
+		var rotation = Angle.degrees(0)
+		var offset = CGSize.zero
+		var blendMode = BlendMode.normal
+		var opacity: Double = 1
+		var fontSize: Double = 1
+	}
+
+	/// Defines a state for the `Ingredient` to transition from when changing between card and thumbnail
+	struct Crop {
+		var xOffset: Double = 0
+		var yOffset: Double = 0
+		var scale: Double = 1
+
+		var offset: CGSize {
+			CGSize(width: xOffset, height: yOffset)
+		}
+	}
+
+	/// The `Ingredient`'s image, useful for backgrounds or thumbnails
+	var image: Image {
+		Image("ingredient/\(id)", label: Text(name))
+			.renderingMode(.original)
+	}
+}
+
+struct Ingredient: Identifiable, Codable {
+	var id: String
+	var name: String
+	var title = CardTitle()
+	var thumbnailCrop = Crop()
+	var cardCrop = Crop()
+
+	enum CodingKeys: String, CodingKey {
+		case id
+		case name
+	}
+}
+
+enum FlipViewSide {
+	case front
+	case back
+
+	mutating func toggle() {
+		self = self == .front ? .back : .front
+	}
+}
+
+public protocol DisplayableMeasurement {
+	var unitImage: Image { get }
+	func localizedSummary(unitStyle: MeasurementFormatter.UnitStyle, unitOptions: MeasurementFormatter.UnitOptions) -> String
+}
+
+extension DisplayableMeasurement {
+	public func localizedSummary() -> String {
+		localizedSummary(unitStyle: .long, unitOptions: [.providedUnit])
+	}
+}
+
+extension Measurement: DisplayableMeasurement {
+	public func localizedSummary(unitStyle: MeasurementFormatter.UnitStyle = .long,
+								 unitOptions: MeasurementFormatter.UnitOptions = [.providedUnit]) -> String {
+		let formatter = MeasurementFormatter()
+		formatter.unitStyle = unitStyle
+		formatter.unitOptions = unitOptions
+		return formatter.string(from: self)
+	}
+
+	public var unitImage: Image {
+		unit.unitIcon
+	}
+}
+
+
+public struct Nutrition: Identifiable {
+	public var id: String
+	public var name: LocalizedStringKey
+	public var measurement: DisplayableMeasurement
+	public var indented: Bool = false
+}
+
+extension NutritionFact {
+	public var nutritions: [Nutrition] {
+		[
+			Nutrition(
+				id: "totalFat",
+				name: "Total Fat",
+				measurement: totalFat
+			),
+			Nutrition(
+				id: "totalSaturatedFat",
+				name: "Saturated Fat",
+				measurement: totalSaturatedFat,
+				indented: true
+			),
+			Nutrition(
+				id: "totalMonounsaturatedFat",
+				name: "Monounsaturated Fat",
+				measurement: totalMonounsaturatedFat,
+				indented: true
+			),
+			Nutrition(
+				id: "totalPolyunsaturatedFat",
+				name: "Polyunsaturated Fat",
+				measurement: totalPolyunsaturatedFat,
+				indented: true
+			),
+			Nutrition(
+				id: "cholesterol",
+				name: "Cholesterol",
+				measurement: cholesterol
+			),
+			Nutrition(
+				id: "sodium",
+				name: "Sodium",
+				measurement: sodium
+			),
+			Nutrition(
+				id: "totalCarbohydrates",
+				name: "Total Carbohydrates",
+				measurement: totalCarbohydrates
+			),
+			Nutrition(
+				id: "dietaryFiber",
+				name: "Dietary Fiber",
+				measurement: dietaryFiber,
+				indented: true
+			),
+			Nutrition(
+				id: "sugar",
+				name: "Sugar",
+				measurement: sugar,
+				indented: true
+			),
+			Nutrition(
+				id: "protein",
+				name: "Protein",
+				measurement: protein
+			),
+			Nutrition(
+				id: "calcium",
+				name: "Calcium",
+				measurement: calcium
+			),
+			Nutrition(
+				id: "potassium",
+				name: "Potassium",
+				measurement: potassium
+			),
+			Nutrition(
+				id: "vitaminA",
+				name: "Vitamin A",
+				measurement: vitaminA
+			),
+			Nutrition(
+				id: "vitaminC",
+				name: "Vitamin C",
+				measurement: vitaminC
+			),
+			Nutrition(
+				id: "iron",
+				name: "Iron",
+				measurement: iron
+			)
+		]
+	}
+}
+
+
+public struct NutritionRow: View {
+	public var nutrition: Nutrition
+
+	public init(nutrition: Nutrition) {
+		self.nutrition = nutrition
+	}
+
+	var nutritionValue: String {
+		nutrition.measurement.localizedSummary(
+			unitStyle: .short,
+			unitOptions: .providedUnit
+		)
+	}
+
+	public var body: some View {
+		HStack {
+			Text(nutrition.name)
+				.fontWeight(.medium)
+			Spacer()
+			Text(nutritionValue)
+				.fontWeight(.semibold)
+				.foregroundStyle(.secondary)
+		}
+		.font(.footnote)
+	}
+}
+
+public struct NutritionFactView: View {
+
+	public var nutritionFact: NutritionFact
+
+	public init(nutritionFact: NutritionFact) {
+		self.nutritionFact = nutritionFact.converted(toVolume: .cups(1))
+	}
+
+	public var body: some View {
+		VStack(alignment: .leading, spacing: 0) {
+			VStack(alignment: .leading) {
+				Text("Nutrition Facts")
+					.font(.title2)
+					.bold()
+				Text("Serving Size 1 Cup")
+					.font(.footnote)
+				Text(nutritionFact.energy.formatted(.measurement(width: .wide, usage: .food)))
+					.fontWeight(.semibold)
+					.padding(.top, 10)
+			}
+			.padding(20)
+
+			Divider()
+				.padding(.horizontal, 20)
+
+			ScrollView {
+				VStack(spacing: 0) {
+					ForEach(nutritionFact.nutritions) { nutrition in
+						NutritionRow(nutrition: nutrition)
+							.padding(.vertical, 4)
+							.padding(.leading, nutrition.indented ? 10 : 0)
+						Divider()
+					}
+				}
+				.padding([.bottom, .horizontal], 20)
+			}
+		}
+	}
+}
+
+extension View {
+	func animatableFont(size: Double, weight: Font.Weight = .regular, design: Font.Design = .default) -> some View {
+		self.modifier(AnimatableFontModifier(size: size, weight: weight, design: design))
+	}
+}
+
+struct IngredientGraphic: View {
+	var ingredient: Ingredient
+	var style: Style
+	var closeAction: () -> Void = {}
+	var flipAction: () -> Void = {}
+
+	enum Style {
+		case cardFront
+		case cardBack
+		case thumbnail
+	}
+
+	var displayingAsCard: Bool {
+		style == .cardFront || style == .cardBack
+	}
+
+	var shape = RoundedRectangle(cornerRadius: 16, style: .continuous)
+
+	var body: some View {
+		ZStack {
+			image
+			if style != .cardBack {
+				title
+			}
+
+			if style == .cardFront {
+				cardControls(for: .front)
+					.foregroundStyle(ingredient.title.color)
+					.opacity(ingredient.title.opacity)
+					.blendMode(ingredient.title.blendMode)
+			}
+
+			if style == .cardBack {
+				ZStack {
+					if let nutritionFact = ingredient.nutritionFact {
+						NutritionFactView(nutritionFact: nutritionFact)
+							.padding(.bottom, 70)
+					}
+					cardControls(for: .back)
+				}
+				.background(.thinMaterial)
+			}
+		}
+		.frame(minWidth: 130, maxWidth: 400, maxHeight: 500)
+		.compositingGroup()
+		.clipShape(shape)
+		.overlay {
+			shape
+				.inset(by: 0.5)
+				.stroke(.quaternary, lineWidth: 0.5)
+		}
+		.contentShape(shape)
+		.accessibilityElement(children: .contain)
+	}
+
+	var image: some View {
+		GeometryReader { geo in
+			ingredient.image
+				.resizable()
+				.aspectRatio(contentMode: .fill)
+				.frame(width: geo.size.width, height: geo.size.height)
+				.scaleEffect(displayingAsCard ? ingredient.cardCrop.scale : ingredient.thumbnailCrop.scale)
+				.offset(displayingAsCard ? ingredient.cardCrop.offset : ingredient.thumbnailCrop.offset)
+				.frame(width: geo.size.width, height: geo.size.height)
+				.scaleEffect(x: style == .cardBack ? -1 : 1)
+		}
+		.accessibility(hidden: true)
+	}
+
+	var title: some View {
+		Text(ingredient.name.uppercased())
+			.padding(.horizontal, 8)
+			.frame(maxWidth: .infinity, maxHeight: .infinity)
+			.lineLimit(2)
+			.multilineTextAlignment(.center)
+			.foregroundStyle(ingredient.title.color)
+			.rotationEffect(displayingAsCard ? ingredient.title.rotation: .degrees(0))
+			.opacity(ingredient.title.opacity)
+			.blendMode(ingredient.title.blendMode)
+			.animatableFont(size: displayingAsCard ? ingredient.title.fontSize : 40, weight: .bold)
+			.minimumScaleFactor(0.25)
+			.offset(displayingAsCard ? ingredient.title.offset : .zero)
+	}
+
+	func cardControls(for side: FlipViewSide) -> some View {
+		VStack {
+			if side == .front {
+				CardActionButton(label: "Close", systemImage: "xmark.circle.fill", action: closeAction)
+					.scaleEffect(displayingAsCard ? 1 : 0.5)
+					.opacity(displayingAsCard ? 1 : 0)
+			}
+			Spacer()
+			CardActionButton(
+				label: side == .front ? "Open Nutrition Facts" : "Close Nutrition Facts",
+				systemImage: side == .front ? "info.circle.fill" : "arrow.left.circle.fill",
+				action: flipAction
+			)
+			.scaleEffect(displayingAsCard ? 1 : 0.5)
+			.opacity(displayingAsCard ? 1 : 0)
+		}
+		.frame(maxWidth: .infinity, alignment: .trailing)
+	}
+}
+
+struct CardActionButton: View {
+	var label: LocalizedStringKey
+	var systemImage: String
+	var action: () -> Void
+
+	var body: some View {
+		Button(action: action) {
+			Image(systemName: systemImage)
+				.font(Font.title.bold())
+				.imageScale(.large)
+				.frame(width: 44, height: 44)
+				.padding()
+				.contentShape(Rectangle())
+		}
+		.buttonStyle(SquishableButtonStyle(fadeOnPress: false))
+		.accessibility(label: Text(label))
+	}
+}
+
+struct SquishableButtonStyle: ButtonStyle {
+	var fadeOnPress = true
+
+	func makeBody(configuration: Configuration) -> some View {
+		configuration.label
+			.opacity(configuration.isPressed && fadeOnPress ? 0.75 : 1)
+			.scaleEffect(configuration.isPressed ? 0.95 : 1)
+	}
+}
+
+extension ButtonStyle where Self == SquishableButtonStyle {
+	static var squishable: SquishableButtonStyle {
+		SquishableButtonStyle()
+	}
+
+	static func squishable(fadeOnPress: Bool = true) -> SquishableButtonStyle {
+		SquishableButtonStyle(fadeOnPress: fadeOnPress)
+	}
+}
---
+++ TypeSlide/Samples/FrutialModel.swift
@@ -0,0 +1,505 @@
+//
+//  FrutialModel.swift
+//  TypeSlide
+//
+//  Created by Misha Nya on 16.04.2024.
+//
+
+import Foundation
+
+public struct Density {
+	public var mass: Measurement<UnitMass>
+	public var volume: Measurement<UnitVolume>
+
+	public init(_ massAmount: Double, _ massUnit: UnitMass, per volumeAmount: Double, _ volumeUnit: UnitVolume) {
+		self.mass = Measurement(value: massAmount, unit: massUnit)
+		self.volume = Measurement(value: volumeAmount, unit: volumeUnit)
+	}
+
+	public init(mass: Measurement<UnitMass>, volume: Measurement<UnitVolume>) {
+		self.mass = mass
+		self.volume = volume
+	}
+}
+
+public struct NutritionFact {
+	public var identifier: String
+	public var localizedFoodItemName: String
+
+	public var referenceMass: Measurement<UnitMass>
+
+	public var density: Density
+
+	public var totalSaturatedFat: Measurement<UnitMass>
+	public var totalMonounsaturatedFat: Measurement<UnitMass>
+	public var totalPolyunsaturatedFat: Measurement<UnitMass>
+	public var totalFat: Measurement<UnitMass> {
+		return totalSaturatedFat + totalMonounsaturatedFat + totalPolyunsaturatedFat
+	}
+
+	public var cholesterol: Measurement<UnitMass>
+	public var sodium: Measurement<UnitMass>
+
+	public var totalCarbohydrates: Measurement<UnitMass>
+	public var dietaryFiber: Measurement<UnitMass>
+	public var sugar: Measurement<UnitMass>
+
+	public var protein: Measurement<UnitMass>
+
+	public var calcium: Measurement<UnitMass>
+	public var potassium: Measurement<UnitMass>
+
+	public var vitaminA: Measurement<UnitMass>
+	public var vitaminC: Measurement<UnitMass>
+	public var iron: Measurement<UnitMass>
+}
+
+extension NutritionFact {
+	public func converted(toVolume newReferenceVolume: Measurement<UnitVolume>) -> NutritionFact {
+		let newRefMassInCups = newReferenceVolume.converted(to: .cups).value
+		let oldRefMassInCups = referenceMass.convertedToVolume(usingDensity: self.density).value
+
+		let scaleFactor = newRefMassInCups / oldRefMassInCups
+
+		return NutritionFact(
+			identifier: identifier,
+			localizedFoodItemName: localizedFoodItemName,
+			referenceMass: referenceMass * scaleFactor,
+			density: density,
+			totalSaturatedFat: totalSaturatedFat * scaleFactor,
+			totalMonounsaturatedFat: totalMonounsaturatedFat * scaleFactor,
+			totalPolyunsaturatedFat: totalPolyunsaturatedFat * scaleFactor,
+			cholesterol: cholesterol * scaleFactor,
+			sodium: sodium * scaleFactor,
+			totalCarbohydrates: totalCarbohydrates * scaleFactor,
+			dietaryFiber: dietaryFiber * scaleFactor,
+			sugar: sugar * scaleFactor,
+			protein: protein * scaleFactor,
+			calcium: calcium * scaleFactor,
+			potassium: potassium * scaleFactor,
+			vitaminA: vitaminA * scaleFactor,
+			vitaminC: vitaminC * scaleFactor,
+			iron: iron * scaleFactor
+		)
+	}
+
+	public func converted(toMass newReferenceMass: Measurement<UnitMass>) -> NutritionFact {
+		let newRefMassInGrams = newReferenceMass.converted(to: .grams).value
+		let oldRefMassInGrams = referenceMass.converted(to: .grams).value
+		let scaleFactor = newRefMassInGrams / oldRefMassInGrams
+		return NutritionFact(
+			identifier: identifier,
+			localizedFoodItemName: localizedFoodItemName,
+			referenceMass: newReferenceMass,
+			density: density,
+			totalSaturatedFat: totalSaturatedFat * scaleFactor,
+			totalMonounsaturatedFat: totalMonounsaturatedFat * scaleFactor,
+			totalPolyunsaturatedFat: totalPolyunsaturatedFat * scaleFactor,
+			cholesterol: cholesterol * scaleFactor,
+			sodium: sodium * scaleFactor,
+			totalCarbohydrates: totalCarbohydrates * scaleFactor,
+			dietaryFiber: dietaryFiber * scaleFactor,
+			sugar: sugar * scaleFactor,
+			protein: protein * scaleFactor,
+			calcium: calcium * scaleFactor,
+			potassium: potassium * scaleFactor,
+			vitaminA: vitaminA * scaleFactor,
+			vitaminC: vitaminC * scaleFactor,
+			iron: iron * scaleFactor
+		)
+	}
+
+	public func amount(_ mass: Measurement<UnitMass>) -> NutritionFact {
+		return converted(toMass: mass)
+	}
+
+	public func amount(_ volume: Measurement<UnitVolume>) -> NutritionFact {
+		let mass = volume.convertedToMass(usingDensity: density)
+		return converted(toMass: mass)
+	}
+}
+
+extension NutritionFact {
+	/// Nutritional information is for 100 grams, unless a `mass` is specified.
+	public static func lookupFoodItem(_ foodItemIdentifier: String,
+									  forMass mass: Measurement<UnitMass> = Measurement(value: 100, unit: .grams)) -> NutritionFact? {
+		return nutritionFacts[foodItemIdentifier]?.converted(toMass: mass)
+	}
+
+	/// Nutritional information is for 1 cup, unless a `volume` is specified.
+	public static func lookupFoodItem(_ foodItemIdentifier: String,
+									  forVolume volume: Measurement<UnitVolume> = Measurement(value: 1, unit: .cups)) -> NutritionFact? {
+		guard let nutritionFact = nutritionFacts[foodItemIdentifier] else {
+			return nil
+		}
+
+		// Convert volume to mass via density
+		let mass = volume.convertedToMass(usingDensity: nutritionFact.density)
+		return nutritionFact.converted(toMass: mass)
+	}
+
+	// MARK: - Examples
+
+	public static var banana: NutritionFact {
+		nutritionFacts["banana"]!
+	}
+
+	public static var blueberry: NutritionFact {
+		nutritionFacts["blueberry"]!
+	}
+
+	public static var peanutButter: NutritionFact {
+		nutritionFacts["peanut-butter"]!
+	}
+
+	public static var almondMilk: NutritionFact {
+		nutritionFacts["almond-milk"]!
+	}
+
+	public static var zero: NutritionFact {
+		NutritionFact(
+			identifier: "",
+			localizedFoodItemName: "",
+			referenceMass: .grams(0),
+			density: Density(mass: .grams(1), volume: .cups(1)),
+			totalSaturatedFat: .grams(0),
+			totalMonounsaturatedFat: .grams(0),
+			totalPolyunsaturatedFat: .grams(0),
+			cholesterol: .grams(0),
+			sodium: .grams(0),
+			totalCarbohydrates: .grams(0),
+			dietaryFiber: .grams(0),
+			sugar: .grams(0),
+			protein: .grams(0),
+			calcium: .grams(0),
+			potassium: .grams(0),
+			vitaminA: .grams(0),
+			vitaminC: .grams(0),
+			iron: .grams(0)
+		)
+	}
+}
+
+extension NutritionFact {
+	// Support adding up nutritional facts
+	public static func + (lhs: NutritionFact, rhs: NutritionFact) -> NutritionFact {
+		// Calculate combined mass, volume and density
+		let totalMass = lhs.referenceMass + rhs.referenceMass
+		let lhsVolume = lhs.referenceMass.convertedToVolume(usingDensity: lhs.density)
+		let rhsVolume = lhs.referenceMass.convertedToVolume(usingDensity: lhs.density)
+		let totalVolume = lhsVolume + rhsVolume
+
+		return NutritionFact(
+			identifier: "",
+			localizedFoodItemName: "",
+			referenceMass: totalMass,
+			density: Density(mass: totalMass, volume: totalVolume),
+			totalSaturatedFat: lhs.totalSaturatedFat + rhs.totalSaturatedFat,
+			totalMonounsaturatedFat: lhs.totalMonounsaturatedFat + rhs.totalMonounsaturatedFat,
+			totalPolyunsaturatedFat: lhs.totalPolyunsaturatedFat + rhs.totalPolyunsaturatedFat,
+			cholesterol: lhs.cholesterol + rhs.cholesterol,
+			sodium: lhs.sodium + rhs.sodium,
+			totalCarbohydrates: lhs.totalCarbohydrates + rhs.totalCarbohydrates,
+			dietaryFiber: lhs.dietaryFiber + rhs.dietaryFiber,
+			sugar: lhs.sugar + rhs.sugar,
+			protein: lhs.protein + rhs.protein,
+			calcium: lhs.calcium + rhs.calcium,
+			potassium: lhs.potassium + rhs.potassium,
+			vitaminA: lhs.vitaminA + rhs.vitaminA,
+			vitaminC: lhs.vitaminC + rhs.vitaminC,
+			iron: lhs.iron + rhs.iron
+		)
+	}
+}
+
+// MARK: - CustomStringConvertible
+
+extension NutritionFact: CustomStringConvertible {
+	public var description: String {
+		let suffix = identifier.isEmpty ? "" : " of \(identifier)"
+		return "\(referenceMass.converted(to: .grams).localizedSummary(unitStyle: .medium))" + suffix
+	}
+}
+
+// MARK: - Volume <-> Mass Conversion
+
+extension Measurement where UnitType == UnitVolume {
+	public func convertedToMass(usingDensity density: Density) -> Measurement<UnitMass> {
+		let densityLiters = density.volume.converted(to: .liters).value
+		let liters = converted(to: .liters).value
+		let scale = liters / densityLiters
+		return density.mass * scale
+	}
+}
+
+extension Measurement where UnitType == UnitMass {
+	public func convertedToVolume(usingDensity density: Density) -> Measurement<UnitVolume> {
+		let densityKilograms = density.mass.converted(to: .kilograms).value
+		let kilograms = converted(to: .kilograms).value
+		let scale = kilograms / densityKilograms
+		return density.volume * scale
+	}
+}
+
+// MARK: - Convenience Accessors
+
+extension Measurement where UnitType == UnitVolume {
+	public static func cups(_ cups: Double) -> Measurement<UnitVolume> {
+		return Measurement(value: cups, unit: .cups)
+	}
+
+	public static func tablespoons(_ tablespoons: Double) -> Measurement<UnitVolume> {
+		return Measurement(value: tablespoons, unit: .tablespoons)
+	}
+}
+
+extension Measurement where UnitType == UnitMass {
+	public static func grams(_ grams: Double) -> Measurement<UnitMass> {
+		return Measurement(value: grams, unit: .grams)
+	}
+}
+
+let nutritionFacts: [String: NutritionFact] = {
+		if let jsonURL = Bundle.main.url(forResource: "NutritionalItems", withExtension: "json"),
+		   let jsonData = try? Data(contentsOf: jsonURL),
+		   let facts = try? JSONDecoder().decode(Dictionary<String, NutritionFact>.self, from: jsonData) {
+			return facts
+		} else {
+			return [String: NutritionFact]()
+		}
+	}()
+
+extension NutritionFact: Decodable {
+	enum CodingKeys: String, CodingKey {
+		case identifier
+		case localizedFoodItemName
+		case referenceMass
+		case density
+		case totalSaturatedFat
+		case totalMonounsaturatedFat
+		case totalPolyunsaturatedFat
+		case cholesterol
+		case sodium
+		case totalCarbohydrates
+		case dietaryFiber
+		case sugar
+		case protein
+		case calcium
+		case potassium
+		case vitaminA
+		case vitaminC
+		case iron
+	}
+
+	public init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+
+		identifier = try values.decode(String.self, forKey: .identifier)
+		localizedFoodItemName = try values.decode(String.self, forKey: .localizedFoodItemName)
+
+		let densityString = try values.decode(String.self, forKey: .density)
+		density = Density.fromString(densityString)
+
+		referenceMass = try values.decode(Measurement<UnitMass>.self, forKey: .referenceMass)
+		totalSaturatedFat = try values.decode(Measurement<UnitMass>.self, forKey: .totalSaturatedFat)
+		totalMonounsaturatedFat = try values.decode(Measurement<UnitMass>.self, forKey: .totalMonounsaturatedFat)
+		totalPolyunsaturatedFat = try values.decode(Measurement<UnitMass>.self, forKey: .totalPolyunsaturatedFat)
+		cholesterol = try values.decode(Measurement<UnitMass>.self, forKey: .cholesterol)
+		sodium = try values.decode(Measurement<UnitMass>.self, forKey: .sodium)
+		totalCarbohydrates = try values.decode(Measurement<UnitMass>.self, forKey: .totalCarbohydrates)
+		dietaryFiber = try values.decode(Measurement<UnitMass>.self, forKey: .dietaryFiber)
+		sugar = try values.decode(Measurement<UnitMass>.self, forKey: .sugar)
+		protein = try values.decode(Measurement<UnitMass>.self, forKey: .protein)
+		calcium = try values.decode(Measurement<UnitMass>.self, forKey: .calcium)
+		potassium = try values.decode(Measurement<UnitMass>.self, forKey: .potassium)
+		vitaminA = try values.decode(Measurement<UnitMass>.self, forKey: .vitaminA)
+		vitaminC = try values.decode(Measurement<UnitMass>.self, forKey: .vitaminC)
+		iron = try values.decode(Measurement<UnitMass>.self, forKey: .iron)
+	}
+}
+
+extension KeyedDecodingContainer {
+	public func decode(_ type: Measurement<UnitMass>.Type, forKey key: KeyedDecodingContainer<K>.Key) throws -> Measurement<UnitMass> {
+		let valueString = try decode(String.self, forKey: key)
+		return Measurement(string: valueString)
+	}
+
+	public func decode(_ type: Measurement<UnitVolume>.Type, forKey key: KeyedDecodingContainer<K>.Key) throws -> Measurement<UnitVolume> {
+		let valueString = try decode(String.self, forKey: key)
+		return Measurement(string: valueString)
+	}
+}
+
+extension Measurement where UnitType == UnitMass {
+	init(string: String) {
+		let components = string.split(separator: " ")
+		let valueString = String(components[0])
+		let unitSymbolString = String(components[1])
+		self.init(
+			value: Double(valueString)!,
+			unit: UnitMass.fromSymbol(unitSymbolString)
+		)
+	}
+}
+
+extension Measurement where UnitType == UnitVolume {
+	init(string: String) {
+		let components = string.split(separator: " ")
+		let valueString = String(components[0])
+		let unitSymbolString = String(components[1])
+		self.init(
+			value: Double(valueString)!,
+			unit: UnitVolume.fromSymbol(unitSymbolString)
+		)
+	}
+}
+
+// These methods return Units with an associated converter, whereas units
+// initialized from a string using the `Unit(symbol:)` initializer
+// do not have a converter associated and thereby don't support conversion.
+
+extension UnitVolume {
+	static func fromSymbol(_ symbol: String) -> UnitVolume {
+		switch symbol {
+		case "gal":
+			return .gallons
+		case "cup":
+			return .cups
+		default:
+			return UnitVolume(symbol: symbol)
+		}
+	}
+}
+
+extension UnitMass {
+	static func fromSymbol(_ symbol: String) -> UnitMass {
+		switch symbol {
+		case "kg":
+			return .kilograms
+		case "g":
+			return .grams
+		case "mg":
+			return .milligrams
+		default:
+			return UnitMass(symbol: symbol)
+		}
+	}
+}
+
+extension UnitEnergy {
+	static func fromSymbol(_ symbol: String) -> UnitEnergy {
+		switch symbol {
+		case "kCal":
+			return .kilocalories
+		default:
+			return UnitEnergy(symbol: symbol)
+		}
+	}
+}
+
+extension Density {
+	fileprivate static func fromString(_ string: String) -> Density {
+		// Example: 5 g per 1 cup
+		let components = string.split(separator: " ")
+
+		let massValue = Double(components[0])!
+		let massUnit: UnitMass = .fromSymbol(String(components[1]))
+		let volumeValue = Double(components[3])!
+		let volumeUnit: UnitVolume = .fromSymbol(String(components[4]))
+
+		return Density(massValue, massUnit, per: volumeValue, volumeUnit)
+	}
+}
+
+import SwiftUI
+
+extension Unit {
+	public var unitIcon: Image {
+		if let iconProvider = self as? UnitIconProvider {
+			return iconProvider.customUnitIcon
+		}
+		// Fallback to 'gauge' where no icon is specified
+		return Image(systemName: "gauge")
+	}
+}
+
+// Allow Unit subclasses to provide icon overrides
+private protocol UnitIconProvider {
+	var customUnitIcon: Image { get }
+}
+
+extension UnitMass: UnitIconProvider {
+	var customUnitIcon: Image {
+		Image(systemName: "scalemass.fill")
+	}
+}
+
+extension UnitVolume: UnitIconProvider {
+	var customUnitIcon: Image {
+		switch symbol {
+		// Icons included in the asset catalog
+		case "cup", "qt", "tbsp", "tsp", "gal":
+			return Image(symbol)
+		default:
+			return Image(systemName: "drop.fill")
+		}
+	}
+}
+
+private let kilocaloriesInFat: Double = 9
+private let kilocaloriesInCarb: Double = 4
+private let kilocaloriesInProtein: Double = 4
+
+public struct CalorieBreakdown {
+	public let percentFat: Double
+	public let percentCarbohydrate: Double
+	public let percentProtein: Double
+
+	public var labeledValues: [(String, Double)] {
+		return [
+			("Protein", percentProtein),
+			("Fat", percentFat),
+			("Carbohydrates", percentCarbohydrate)
+		]
+	}
+}
+
+extension NutritionFact {
+	public var kilocaloriesFromFat: Double {
+		totalFat.converted(to: .grams).value * kilocaloriesInFat
+	}
+
+	public var kilocaloriesFromCarbohydrates: Double {
+		(totalCarbohydrates - dietaryFiber).converted(to: .grams).value * kilocaloriesInCarb
+	}
+
+	public var kilocaloriesFromProtein: Double {
+		protein.converted(to: .grams).value * kilocaloriesInProtein
+	}
+
+	public var kilocalories: Double {
+		kilocaloriesFromFat + kilocaloriesFromCarbohydrates + kilocaloriesFromProtein
+	}
+
+	public var energy: Measurement<UnitEnergy> {
+		return Measurement<UnitEnergy>(value: kilocalories, unit: .kilocalories)
+	}
+
+	public var calorieBreakdown: CalorieBreakdown {
+		let totalKilocalories = kilocalories
+		let percentFat = kilocaloriesFromFat / totalKilocalories * 100
+		let percentCarbohydrate = kilocaloriesFromCarbohydrates / totalKilocalories * 100
+		let percentProtein = kilocaloriesFromProtein / totalKilocalories * 100
+		return CalorieBreakdown(
+			percentFat: percentFat,
+			percentCarbohydrate: percentCarbohydrate,
+			percentProtein: percentProtein
+		)
+	}
+}
+
+extension Ingredient {
+	var nutritionFact: NutritionFact? {
+		NutritionFact.lookupFoodItem(id, forVolume: .cups(1))
+	}
+}
